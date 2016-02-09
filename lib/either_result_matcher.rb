a553f0ed6ef8299c68968715f18bb404dc05b9b8
Tim Riley <tim@openmonkey.com>
2016-02-09 22:02:32 +1100

    Add `EitherResultMatcher.for(*methods)` for including in other classes

--- lib/either_result_matcher.rb
+++ lib/either_result_matcher.rb
@@ -3,3 +3,29 @@ require "either_result_matcher/matcher"
 def MatchEitherResult(result, &block)
   block.call(EitherResultMatcher::Matcher.new(result))
 end
+
+module EitherResultMatcher
+  def self.for(*match_methods)
+    matchers_mod = Module.new do
+      match_methods.each do |match_method|
+        define_method(match_method) do |*args, &block|
+          result = super(*args)
+
+          if block
+            MatchEitherResult(result, &block)
+          else
+            result
+          end
+        end
+      end
+    end
+
+    Module.new do
+      const_set :Matchers, matchers_mod
+
+      def self.included(klass)
+        klass.prepend const_get(:Matchers)
+      end
+    end
+  end
+end
