bb217e46e052c2fa4e58d20c43a7cebc38c84816
Tim Riley <tim@openmonkey.com>
2016-06-28 22:15:04 +0930

    Offer match cases in Evaluator via method_missing
    This means we don’t blow away Ruby’s method cache by defining new methods for each case.

--- lib/dry/result_matcher/evaluator.rb
+++ lib/dry/result_matcher/evaluator.rb
@@ -1,39 +1,37 @@
 module Dry
   module ResultMatcher
-    class Evaluator
-      attr_reader :result
-      attr_reader :output
-
+    class Evaluator < BasicObject
       def initialize(result, cases)
+        @cases = cases
         @result = result
-        @output = nil
         @matched = false
-
-        cases.each do |name, kase|
-          define_singleton_method(name) do |pattern = nil, &block|
-            handle_case(kase, pattern, &block)
-          end
-        end
+        @output = nil
       end
 
       def call(&block)
         block.call(self)
-        output
+        @output
+      end
+
+      def respond_to_missing?(name, include_private = false)
+        @cases.key?(name) || super
+      end
+
+      def method_missing(name, *args, &block)
+        return super unless @cases.key?(name)
+
+        handle_case @cases[name], *args, &block
       end
 
       private
 
-      def handle_case(kase, pattern, &block)
-        return(output) if matched?
+      def handle_case(kase, pattern = nil, &block)
+        return @output if @matched
 
-        if kase.matches?(pattern, result)
+        if kase.matches?(pattern, @result)
           @matched = true
-          @output = block.call(kase.resolve(result))
+          @output = block.call(kase.resolve(@result))
         end
-      end
-
-      def matched?
-        !!@matched
       end
     end
   end
