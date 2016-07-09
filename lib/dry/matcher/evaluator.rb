649db5f671d340542aff5237d58b7a13c19cc921
Tim Riley <tim@openmonkey.com>
2016-07-09 21:27:58 +0930

    Require exhaustive matches

--- lib/dry/matcher/evaluator.rb
+++ lib/dry/matcher/evaluator.rb
@@ -1,15 +1,24 @@
+require "set"
+
 module Dry
   class Matcher
+    NonExhaustiveMatchError = Class.new(StandardError)
+
     class Evaluator < BasicObject
       def initialize(result, cases)
         @cases = cases
         @result = result
+
+        @handled = ::Set.new
         @matched = false
         @output = nil
       end
 
       def call
         yield self
+
+        ensure_exhaustive_match
+
         @output
       end
 
@@ -20,10 +29,17 @@ module Dry
       def method_missing(name, *args, &block)
         return super unless @cases.key?(name)
 
+        @handled.add name
         handle_case @cases[name], *args, &block
       end
 
       private
+
+      def ensure_exhaustive_match
+        if (not_handled = @cases.keys - @handled.to_a).any?
+          ::Kernel.raise NonExhaustiveMatchError, "cases +#{not_handled.join(', ')}+ not handled"
+        end
+      end
 
       def handle_case(kase, *pattern)
         return @output if @matched
