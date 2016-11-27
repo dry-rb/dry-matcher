1d5cb26c94a25db8844853d518e353139ea83703
Alex Semyonov <alex@semyonov.us>
2016-11-27 15:41:17 +0300

    Document Dry::Matcher

--- lib/dry/matcher.rb
+++ lib/dry/matcher.rb
@@ -2,7 +2,30 @@ require "dry/matcher/case"
 require "dry/matcher/evaluator"
 
 module Dry
+  # @see http://dry-rb.org/gems/dry-matcher
   class Matcher
+    # Generates a module containing pattern matching for methods listed in
+    # `match_methods` argument with behavior defined by `with` matcher
+    #
+    # @param [<Symbol>] match_methods
+    # @param [Dry::Matcher] with
+    # @return [Module]
+    #
+    # @example Usage with `dry-monads`
+    #   class MonadicOperation
+    #     include Dry::Matcher.for(:call, with: Dry::Matcher::EitherMatcher)
+    #
+    #     def call
+    #       Dry::Monads::Either::Right.new('Success')
+    #     end
+    #   end
+    #
+    #   operation = MonadicOperation.new
+    #
+    #   operation.call do |m|
+    #     m.success { |v| "#{v} was successful!"}
+    #     m.failure { |v| "#{v} has failed!"}
+    #   end #=> "Success was successful"
     def self.for(*match_methods, with:)
       matcher = with
 
@@ -29,12 +52,33 @@ module Dry
       end
     end
 
+    # @return [Hash{Symbol => Case}]
     attr_reader :cases
 
+    # @param [Hash{Symbol => Case}] cases
     def initialize(cases = {})
       @cases = cases
     end
 
+    # Evaluate {#cases}' matchers and returns a result of block given to
+    # corresponding case matcher
+    #
+    # @param [Object] result value that would be tested for matches with given {#cases}
+    # @param [Object] block
+    # @yieldparam [Evaluator] m
+    # @return [Object] value returned from the block given to method called
+    #   after matched pattern
+    #
+    # @example Usage with `dry-monads`
+    #   require 'dry-monads'
+    #   require 'dry/matcher/either_matcher'
+    #
+    #   value = Dry::Monads::Either::Left.new('failure!')
+    #
+    #   Dry::Matcher::EitherMatcher.(value) do |m|
+    #     m.success { |v| "Yay: #{v}" }
+    #     m.failure { |v| "Boo: #{v}" }
+    #   end #=> "Boo: failure!"
     def call(result, &block)
       Evaluator.new(result, cases).call(&block)
     end
