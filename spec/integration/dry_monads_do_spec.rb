053711736bca91c71dbefd6ff1c9177975b363c7
Tim Riley <tim@openmonkey.com>
2018-06-14 08:38:00 +1000

    Check both methods for using Do notation

--- spec/integration/dry_monads_do_spec.rb
+++ spec/integration/dry_monads_do_spec.rb
@@ -3,47 +3,82 @@ require "dry/monads/do"
 require "dry/matcher/result_matcher"
 
 RSpec.describe "Integration with dry-monads Do notation" do
-  let(:operation) {
-    Class.new do
-      include Dry::Monads::Result::Mixin
-      include Dry::Monads::Do
-      include Dry::Matcher.for(:call, with: Dry::Matcher::ResultMatcher)
+  shared_examples "class using both dry-matcher and dry-monads Do notation" do
+    it "supports yielding via Do notation as well as final result matching block" do
+      matched_success = nil
+      matched_failure = nil
 
-      def call(user)
-        user = yield validate(user)
-        greeting = yield greet(user)
-
-        Success(greeting)
+      operation.(name: "Jane", email: "jane@example.com") do |m|
+        m.success { |v| matched_success = v }
+        m.failure { }
       end
 
-      private
-
-      def validate(user)
-        user[:email] ? Success(user) : Failure(:no_email)
+      operation.(name: "Jo") do |m|
+        m.success { }
+        m.failure { |v| matched_failure = v }
       end
 
-      def greet(user)
-        Success("Hello, #{user[:name]}")
-      end
-    end.new
-  }
-
-  it "supports yielding via Do notation as well as final result matching block" do
-    matched_success = nil
-    matched_failure = nil
-
-    operation.(name: "Jane", email: "jane@example.com") do |m|
-      m.success { |v| matched_success = v }
-      m.failure { }
+      expect(matched_success).to eq "Hello, Jane"
+      expect(matched_failure).to eq :no_email
     end
+  end
 
-    operation.(name: "Jo") do |m|
-      m.success { }
-      m.failure { |v| matched_failure = v }
-    end
+  describe "yielding" do
+    let(:operation) {
+      Class.new do
+        include Dry::Monads::Result::Mixin
+        include Dry::Monads::Do
+        include Dry::Matcher.for(:call, with: Dry::Matcher::ResultMatcher)
 
-    expect(matched_success).to eq "Hello, Jane"
-    expect(matched_failure).to eq :no_email
+        def call(user)
+          user = yield validate(user)
+          greeting = yield greet(user)
+
+          Success(greeting)
+        end
+
+        private
+
+        def validate(user)
+          user[:email] ? Success(user) : Failure(:no_email)
+        end
+
+        def greet(user)
+          Success("Hello, #{user[:name]}")
+        end
+      end.new
+    }
+
+    it_behaves_like "class using both dry-matcher and dry-monads Do notation"
+  end
+
+  describe "calling bind block explicitly" do
+    let(:operation) {
+      Class.new do
+        include Dry::Monads::Result::Mixin
+        include Dry::Monads::Do
+        include Dry::Matcher.for(:call, with: Dry::Matcher::ResultMatcher)
+
+        def call(user, &bind)
+          user = bind.(validate(user))
+          greeting = bind.(greet(user))
+
+          Success(greeting)
+        end
+
+        private
+
+        def validate(user)
+          user[:email] ? Success(user) : Failure(:no_email)
+        end
+
+        def greet(user)
+          Success("Hello, #{user[:name]}")
+        end
+      end.new
+    }
+
+    it_behaves_like "class using both dry-matcher and dry-monads Do notation"
   end
 end
 
