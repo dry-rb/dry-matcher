976c11f0d3770cac03facca31066392b49e27b08
Nikita Shilnikov <fg@flashgordon.ru>
2019-07-24 15:57:24 +0300

    Deprecate EitherMatcher

--- lib/dry/matcher/either_matcher.rb
+++ lib/dry/matcher/either_matcher.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
+require 'dry/core/deprecations'
 require 'dry/matcher/result_matcher'
 
 module Dry
   class Matcher
+    extend Dry::Core::Deprecations[:'dry-matcher']
+
     EitherMatcher = ResultMatcher
+
+    deprecate_constant(:EitherMatcher, message: 'Dry::Matcher::ResultMatcher')
   end
 end
